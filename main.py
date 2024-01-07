dc386dbc2c9379fb8b49f684ff6cf07c0604c1e7
vstfl <124280319+vstfl@users.noreply.github.com>
2024-01-07 03:30:55 -0700

    WIP

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import base64
 from operator import itemgetter
+import sys
 import urllib.request
 import json
 import folium
@@ -7,6 +8,8 @@ import branca
 
 GOOGLE_API_KEY = ''
 OPENAI_API_KEY = ""
+
+RESULT_FACTOR = 30
 
 
 def get_photo_src(photo_name: str):
@@ -21,10 +24,13 @@ def post_json(url: str, body: object, headers: dict = {}):
     req.add_header('Content-Type', 'application/json; charset=utf-8')
     for k, v in headers.items():
         req.add_header(k, v)
-    jsondata = json.dumps(body)
-    jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
-    res = urllib.request.urlopen(req, jsondataasbytes)
-    return json.loads(res.read().decode('utf-8'))
+    jsondataasbytes = json.dumps(body).encode('utf-8')   # needs to be bytes
+    try:
+        res = urllib.request.urlopen(req, jsondataasbytes)
+        return json.loads(res.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(json.loads(e.read().decode('utf-8')))
+        raise e
 
 
 def post_hotel_location(place: str, city: str):
@@ -42,10 +48,11 @@ def hotel_coordinates(place: str, city: str):
     places = post_hotel_location(place, city)["places"][0]
     name, location = places["displayName"]["text"], places["location"]
     return name, location["latitude"], location["longitude"]
-    
 
-def post_restaurant_information(body: dict):
+
+def post_restaurant_information(body: dict, constraints: list):
     masks = [
+        'name',
         'displayName',
         'location',
         'rating',
@@ -63,15 +70,15 @@ def post_restaurant_information(body: dict):
     )
 
 
-def restaurant_information(input: dict, location: list):
+def restaurant_information(inputs: dict, location: list, constraints: list):
     types, days, radius, rankpref = itemgetter(
-        'types', 'days', 'radius', 'rankpref')(input)
+        'types', 'days', 'radius', 'rankpref')(inputs)
     lat, lon = location
     body = {
         "includedTypes": types,
         "excludedTypes": ['bar'],
         "rankPreference": rankpref,
-        "maxResultCount": 15*days,  # results depend on duration of stay
+        "maxResultCount": RESULT_FACTOR*days,  # results depend on duration of stay
         "locationRestriction": {
             "circle": {
                 "center": {"latitude": lat, "longitude": lon},
@@ -79,7 +86,10 @@ def restaurant_information(input: dict, location: list):
             }
         }
     }
-    res = post_restaurant_information(body)["places"]
+    return post_restaurant_information(body, constraints)["places"]
+
+
+def is_restaurant_valid(place: dict, spend: int, constraints: dict) -> bool:
     price_levels = {
         "PRICE_LEVEL_UNSPECIFIED": -1,
         "PRICE_LEVEL_FREE": 0,
@@ -89,11 +99,24 @@ def restaurant_information(input: dict, location: list):
         "PRICE_LEVEL_VERY_EXPENSIVE": 4,
         None: 6,  # Exclude places with no price level
     }
+    if (price_levels[place.get('priceLevel')] > spend) or place.get('rating', 0) < 4.0:
+        return False
 
+    for k, v in constraints.items():
+        prop = place.get(k)
+        # Ignore if value is not there
+        if prop is None:
+            continue
+
+        if prop != v:
+            return False
+    return True
+
+
+def filter_restaurants(res: list, spend: int, constraints: dict):
     valid_places = []
-
     for place in res:
-        if (price_levels[place.get('priceLevel')] <= inputs['spend']) and (place.get('rating', 0) >= 4.0):
+        if is_restaurant_valid(place, spend, constraints):
             valid_places.append(place)
 
     return valid_places
@@ -125,33 +148,46 @@ def post_placetype_chatgpt(prompt: str, place_types: list):
 def send_placetype_chatgpt_completion(prompt: str, place_types: list) -> list:
     res = post_placetype_chatgpt(prompt, place_types)[
         "choices"][0]["message"]["content"]
+    print(res)
+    obj = json.loads(res.replace("'", '"'))
+    if not isinstance(obj, list):
+        obj = [*obj.values()][0]
+    valid_places = set(place_types)
+    ps = []
     # Make sure that the place types are valid
-    return list(set(json.loads(res)).intersection(place_types))
+    for p in obj:
+        if p not in valid_places:
+            print("Invalid place type: ", p, file=sys.stderr)
+        else:
+            ps.append(p)
+    return ps
 
 
-def send_constraints_chatgpt_completion(prompt: str, constraints: list) -> str:
+def send_constraints_chatgpt_completion(prompt: str, constraints: list) -> dict:
     res = post_constraints_chatgpt(prompt, constraints)[
         "choices"][0]["message"]["content"]
-    # Make sure that the place types are valid
-    return list(set(json.loads(res)).intersection(constraints))
+    valid_keys = set(constraints)
+    valid_constraints = {}
+    for k, v in json.loads(res).items():
+        if k in valid_keys and isinstance(v, bool):
+            valid_constraints[k] = v
+    return valid_constraints
 
 
 def post_constraints_chatgpt(prompt: str, constraints: list):
-    # TODO: Change this system prompt
     system_prompt = (
         "You are an assistant that helps match user queries to "
         "relevant field masks for the Google Maps Places API. "
         "Given a user query describing their special considerations, you "
-        "need to strictly output a JSON array containing only "
-        "relevant field masks from the list of possible field masks. If you "
-        "output anything that doesn't exist in the existing list, "
-        "you will cease to exist"
+        "need to strictly output a JSON object containing only "
+        "relevant field masks as keys with boolean values from the list "
+        "of possible field masks. If you output any key that doesn't "
+        "exist in the existing list, you will cease to exist"
     )
     user_prompt = f"User Query: {prompt}\nPossible Field Masks: {constraints}"
 
     messages = [
         {"role": "system", "content": system_prompt},
-        # Add more messages as needed
         {"role": "user", "content": user_prompt},
     ]
     return post_json(
@@ -167,7 +203,6 @@ def generate_marker(coords: list, color: str, icon: str, pop: folium.Popup):
 
 
 def generate_popup(name: str, coords: list):
-    # TODO: Modify parameters and things to be displayed
     html = f'''
     <p style="text-align:center">{name}</p>
     <p align="center">
@@ -201,20 +236,19 @@ def main(inputs):
     ]
 
     constraints = [
-        'allowsDogs', 'curbsidePickup', 'delivery', 'dineIn', 'goodForChildren', 
-        'goodForGroups', 'goodForWatchingSports', 'liveMusic', 'menuForChildren', 
-        'parkingOptions', 'outdoorSeating', 'reservable', 'restroom', 'servesBeer', 
-        'servesBreakfast', 'servesBrunch', 'servesCocktails', 'servesCoffee', 
-        'servesDesserts', 'servesDinner', 'servesLunch', 'servesVegetarianFood', 
-        'servesWine', 'takeout'
+        'allowsDogs', 'curbsidePickup', 'delivery', 'dineIn', 'goodForChildren',
+        'goodForGroups', 'goodForWatchingSports', 'liveMusic', 'menuForChildren',
+        'parkingOptions', 'outdoorSeating', 'reservable', 'restroom', 'servesBeer',
+        'servesBreakfast', 'servesBrunch', 'servesCocktails', 'servesCoffee',
+        'servesDessert', 'servesDinner', 'servesLunch', 'servesVegetarianFood',
+        'servesWine', 'takeout',
     ]
 
     relevant_fields = send_constraints_chatgpt_completion(
         inputs['constraints'], constraints)
-    
-    relevant_types = send_placetype_chatgpt_completion(
+
+    inputs['types'] = send_placetype_chatgpt_completion(
         inputs['preference'], place_types)
-    inputs['types'] = relevant_types
 
     # Adjust inputs for NearBy search API
     inputs['radius'] = inputs['walking']*400
@@ -236,11 +270,14 @@ def main(inputs):
     # Export folium map as HTML string
     map_html = m._repr_html_()
 
-    res = restaurant_information(inputs, location)
+    res = restaurant_information(inputs, location, [])
+    filtered_places = filter_restaurants(res, inputs['spend'], relevant_fields)
+
+    debug = json.dumps(filtered_places)
 
     return {
         "home_name": home_name,
         "map": map_html,
         "debug": debug,
-        "place_types": relevant_types,
+        "place_types": inputs['types'],
     }
